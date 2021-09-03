de2017db6199be15dc9bcb89c7fa2f2965925425
Bhanu460 <70477888+Bhanu460@users.noreply.github.com>
2021-09-03 11:24:13 +0530

    Update 12-05-2021 pass code 1.py

--- 12-05-2021 pass code 1.py
+++ 12-05-2021 pass code 1.py
@@ -1,6 +1,6 @@
-for letters in "python":
-    if letters == 'h':
+for letter in "python":
+    if letter == 'h':
         continue
         print('this is pass block')
-    print('current letter:', letters)
+    print('current letter:', letter)
 
